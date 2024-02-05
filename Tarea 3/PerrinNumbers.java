afec6568c425c383f1c63f27ff4da4e81ecaea1a
MRizzo1 <46231629+MRizzo1@users.noreply.github.com>
2024-02-05 13:20:56 -0400

    Adding comments to PerrinNumbers.java

--- Tarea 3/PerrinNumbers.java
+++ Tarea 3/PerrinNumbers.java
@@ -1,18 +1,43 @@
+/**
+ * Programa para obtener los numeros de Perrin aprovechando la version mejorada
+ * de exponenciar una matrix.
+ * 
+ * @author Mariangela Rizzo
+ */
 public class PerrinNumbers {
 
+    /**
+     * Obtiene los numeros de Perrin usando exponenciacion de matrices
+     * 
+     * @param n El Perrin(n) que se quiere obtener
+     * @return El numero de Perrin para n.
+     */
     static int fastPerrin(int n) {
+        // Matrices para obtener el numero de Perrin
         int P[][] = new int[][] { { 0, 1, 0 }, { 0, 0, 1 }, { 1, 1, 0 } };
         int BASE[][] = new int[][] { { 3 }, { 0 }, { 2 } };
+
+        // Manejamos los casos base aparte
         if (n >= 0 && n < 3)
             return BASE[n][0];
 
+        // Exponenciamos la matriz acorde a la formula
         fastExponentiation(P, n - 2);
 
+        // Multiplicamos el resultado por la base y P(n) siemore va a ser el ultimo argumento de la matriz resultante
+        // Es decir, (P(n + 2))
         int result[][] = multiplyBASE(P, BASE);
 
         return result[2][0];
     }
 
+    /**
+     * Version rapida de la exponenciacion de matrices
+     * 
+     * @param P Matriz a exponenciar, regresa exponenciada en si misma
+     * @param n El numero al que queremos exponenciar la matriz
+     * @return No retorna nada
+     */
     static void fastExponentiation(int P[][], int n) {
 
         int BasePCopy[][] = { { 0, 1, 0 }, { 0, 0, 1 }, { 1, 1, 0 } };
@@ -28,6 +53,13 @@ public class PerrinNumbers {
 
     }
 
+    /**
+     * Multiplicamos matrices de la forma 3x3 y 3x1
+     * 
+     * @param M1 Primera matriz a multiplicar de la forma 3x3
+     * @param M2 Segunda matriz a multiplicar de la forma 3x1
+     * @return Resultado de la multiplicacion
+     */
     static int[][] multiplyBASE(int M1[][], int M2[][]) {
 
         int result[][] = new int[3][1];
@@ -43,6 +75,13 @@ public class PerrinNumbers {
 
     }
 
+    /**
+     * Multiplicamos matrices de la forma 3x3 y 3x3
+     * 
+     * @param M1 Primera matriz a multiplicar de la forma 3x3, se retorna el resultado de la multiplicacion sustituyendo esta matriz
+     * @param M2 Segunda matriz a multiplicar de la forma 3x3
+     * @return Nada
+     */
     static void multiply3x3(int M1[][], int M2[][]) {
 
         int result[][] = new int[3][3];
